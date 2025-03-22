6c01f06f637d85d08bd6b472c9991a0311d391ee
GabrielG <ggonza90@asu.edu>
2025-03-21 23:32:15 -0700

    change settings for MySQL

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-1',
+        'USER': 'admin',
+        'PASSWORD': 'Keep!MyDatabase!!Safe!!01',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
