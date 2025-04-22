8a1bc3709dbf7dff6f1626931cca60c3fdd902ed
GabrielG <ggonza90@asu.edu>
2025-04-21 20:27:37 -0700

    Removed because didn't work

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-=l#mona^_a2(*ph5^1*-zk(+2jniv$psm#2+_#c2#c_&*4*i+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Had this blank, remove if it doesn't work
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
