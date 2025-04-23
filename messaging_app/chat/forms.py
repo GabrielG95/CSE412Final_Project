eaef1c00cafc4b60d963bb1f1cdacf8875859fdf
GabrielG <ggonza90@asu.edu>
2025-04-23 09:50:31 -0700

    Fixed set_username.html coloring

--- messaging_app/chat/forms.py
+++ messaging_app/chat/forms.py
@@ -18,7 +18,7 @@ class MessageForm(forms.ModelForm):
 # So users can select a username to display
 class UsernameForm(forms.Form):
     username = forms.CharField(max_length=30, label='Enter your username',
-                               widget=forms.TextInput(attrs={'style': 'color: #ffffff;'}))
+                               widget=forms.TextInput(attrs={'style': 'color: #000000;'}))
 
 class UsernameColorForm(forms.ModelForm):
     class Meta:
